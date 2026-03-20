ac987fcf69aa62a410d0afb84ce33eefcf6ada3c
HTuyenne <nhtuyen102003@gmail.com>
2026-03-21 00:33:25 +0700

    first commit

--- obj/Release/net8.0-windows/WarehouseSystem.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/WarehouseSystem.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WarehouseSystem")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59dfb83811523f946af619eec93038afcb31f965")]
 [assembly: System.Reflection.AssemblyProductAttribute("WarehouseSystem")]
