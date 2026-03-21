6b6fd968be0cf5fa91b466a90f8b97f63c044bc3
HTuyenne <nhtuyen102003@gmail.com>
2026-03-21 17:01:49 +0700

    .

--- obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace WarehouseSystem {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WarehouseSystem;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WarehouseSystem;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
